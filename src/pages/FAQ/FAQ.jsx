75435137f229c5601d5f54df22fc454f4932dda5
SV <vshree852@gmail.com>
2024-11-20 18:14:38 -0800

    update for decaf winter 2025

--- src/pages/FAQ/FAQ.jsx
+++ src/pages/FAQ/FAQ.jsx
@@ -8,8 +8,8 @@ function FAQ() {
       answer: 'DECaF, or the Disciplines of Engineering Career Fair, is an annual, student-run career fair organized by the Triton Engineering Student Council at UC San Diego. It connects engineering students with recruiters from various industries.'
     },
     {
-      question: 'When and where does DECaF 2024 take place?',
-      answer: 'DECaF 2024 will be held on October 9, 2024, from 10 AM to 2 PM at the Qualcomm Conference Center, UC San Diego.'
+      question: 'When and where does DECaF Winter 2025 take place?',
+      answer: 'DECaF Winter 2025 will be held on January 21, 2025, from 10 AM to 2 PM at the Qualcomm Conference Center, UC San Diego.'
     },
     {
       question: 'How can I get more information?',
@@ -20,7 +20,7 @@ function FAQ() {
   const recruiterFaqs = [
     {
       question: 'How can my company participate?',
-      answer: 'Companies interested in participating can find the registration link on the home page. The deadline for registration is September 15th 2024.'
+      answer: 'Companies interested in participating can find the registration link on the home page. The deadline for registration is January 21st 2025.'
     },
     {
       question: 'Are there any costs associated with participating?',
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,22 +7,28 @@ function Home() {
       <Header />
       <div className="content">
         <div className="title">
-          <p id="heading">DECaF 2024</p>
+          <p id="heading">DECaF Winter 2025</p>
           <p id="subheading">Disciplines of Engineering Career Fair</p>
           <p id="venue">Qualcomm Conference Center, UC San Diego</p>
-          <p id="venue">October 9th 2024, 10am - 2pm</p>
+          <p id="venue">January 21st 2025, 10am - 2pm</p>
           <div className="buttons">
             <div className="button-group">
               <div className="button" id="student">
                 Student Registration
               </div>
-              <p className="deadline">Registration Closed</p>
+              <p className="deadline">Registration Opens Soon</p>
             </div>
             <div className="button-group">
               <div className="button" id="company">
-                Company Registration
+              <a
+                  href="https://docs.google.com/forms/d/1yd30YZFGGzJuseup9kkmxMWH64UlgqnSwPr59grt-C4/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Company Registration
+                </a>
               </div>
-              <p className="deadline">Registration Closed</p>
+              <p className="deadline">Registration Open</p>
             </div>
           </div>
         </div>
