5c99a1d4dab4526bd0b02c4ba6a8fc40ba5d6ef5
nselvakumar25 <nselvakumar@ucsd.edu>
2025-01-15 20:59:31 -0800

    add sponsors to sponsors section

--- src/pages/Sponsor/Sponsor.jsx
+++ src/pages/Sponsor/Sponsor.jsx
@@ -1,11 +1,9 @@
 import "./Sponsor.css";
 import CoffeeMug from "../../assets/CoffeeMug.svg";
-// import L3Harris from "../../logos/L3Harris_Logo.png";
-// import Dynovas from "../../logos/Dynovas Matrix Logo (Final).jpg";
-// import LLNL from "../../logos/LLNLlogoBLACK.jpg";
-// import Viasat from "../../logos/Viasat_2018_Gradient_Web (1).jpg";
-// import LANL from "../../logos/LANL_Logo_Temp.png";
-// import NTAG_SW from "../../logos/NTAG_SW_Logo.png";
+import TSMC from "../../logos/TSMC-logo.png";
+import Keller from "../../logos/Keller-North-America.png";
+import Freeform from "../../logos/Freeform-logo.png";
+import Viasat from "../../logos/Viasat_2018_Gradient_Web (1).jpg";
 
 function Sponsor() {
   return (
@@ -16,10 +14,11 @@ function Sponsor() {
       <div className="content">
         <p>Platinum</p>
         <div className="plat">
-          <div>
-            <img src={CoffeeMug} alt="coffee_mug" id="mug"></img>
-            Future Sponsor
-          </div>
+          <a href="http://tsmc.com/english">
+            <div className="sponsor-item">
+              <img src={TSMC} alt="TSMC logo"></img>
+            </div>
+          </a>
         </div>
         <p>Gold</p>
         <div className="gold">
@@ -30,14 +29,21 @@ function Sponsor() {
         </div>
         <p>Silver</p>
         <div className="silver">
-          <div>
-            <img src={CoffeeMug} alt="coffee_mug" id="mug"></img>
-            Future Sponsor
-          </div>
-          <div>
-            <img src={CoffeeMug} alt="coffee_mug" id="mug"></img>
-            Future Sponsor
-          </div>
+          <a href="https://www.keller-na.com">
+            <div className="sponsor-item">
+              <img src={Keller} alt="Keller logo"></img>
+            </div>
+          </a>
+          <a href="https://freeform.co">
+            <div className="sponsor-item" id="bg-black">
+              <img src={Freeform} alt="Freeform logo"></img>
+            </div>
+          </a>
+          <a href="https://www.viasat.com">
+            <div className="sponsor-item">
+              <img src={Viasat} alt="Viasat logo"></img>
+            </div>
+          </a>
         </div>
         <p>Bronze</p>
         <div className="bronze">
